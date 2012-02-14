1bffc3d1f5b386a185c036ad4ac2301663f3d3ad
Istvan Demeter <demeter.istvan@ezit.hu>
2012-02-14 19:01:33 +0100

    stripped, template allow users

--- Modulefile
+++
@@ -1,9 +0,0 @@
-name 'dhoppe-ssh'
-version '0.0.1'
-
-author 'Dennis Hoppe'
-license 'GPLv2'
-project_page 'http://github.com/dhoppe/puppet-ssh'
-source 'git://github.com/dhoppe/puppet-ssh'
-summary 'SSH Module'
-description 'SSH Module for Debian, Ubuntu'
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,22 +1,23 @@
 class ssh {
-	file { "/etc/ssh/sshd_config":
-		owner   => root,
-		group   => root,
-		mode    => 0644,
-		alias   => "sshd_config",
-		source  => "puppet:///modules/ssh/${::lsbdistcodename}/etc/ssh/sshd_config",
-		notify  => Service["ssh"],
-		require => Package["openssh-server"],
-	}
+	define ssh::allow_users($allow_users = false) {
+		$t_allow_users = $allow_users ? {
+			false => [],
+			default => $allow_users,
+		}
 
-	file { "/root/.ssh":
-		force   => true,
-		purge   => true,
-		recurse => true,
-		owner   => root,
-		group   => root,
-		mode    => 0600, 
-		source  => "puppet:///modules/ssh/common/root/.ssh/${::hostname}",
+		file { "$name":
+		    owner   => root,
+		    group   => root,
+		    mode    => 0644,
+		    alias   => "sshd_config",
+		    source  => "puppet:///modules/ssh/${::lsbdistcodename}/etc/ssh/sshd_config",
+		    notify  => Service["ssh"],
+		    require => Package["openssh-server"],
+		}
+	}
+	
+	ssh::allow_users { "/etc/ssh/sshd_config":
+	    allow_users => hiera('allow_users'),
 	}
 
 	package { "openssh-server":
@@ -35,4 +36,3 @@ class ssh {
 	}
 }
 
-# vim: tabstop=3
